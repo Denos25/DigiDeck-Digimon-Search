4f4695bf45c81ce7021479e5653658d3280b5c69
pyrotechnicsid <sidbose93@gmail.com>
2022-04-27 15:22:53 -0500

    final

--- app.js
+++ app.js
@@ -1,50 +1,70 @@
-const searchBtn=document.getElementById('submit')
+const searchBtn = document.getElementById('submit')
 
- searchBtn.addEventListener("click",userinput)
- function userinput()
- {
+searchBtn.addEventListener("click", userinput)
+function userinput() {
     const inputText = document.querySelector('input').value;
-    console.log(inputText)
-    $.get(`https://api.tvmaze.com/search/shows?q=${inputText}`,function(data){
-      if (inputText === ""){
-        alert("Please input a show name!");
-      }
-      else {
-        var results = document.querySelector('#results');
-        results.innerHTML = "";
-        for (var i = 0; i<data.length; i++) {
-          var current = data[i];
-        
-          var span = document.createElement('span');
-          var h3 = document.createElement('h3');
-          h3.className = "card-title";
-          h3.textContent = current.show.name;
-          span.className = 'result-card';
-          span.append(h3);
-          var img = document.createElement('img');
-          img.className = 'card-image';
-          img.setAttribute('src', `${current.show.image.medium}`);
-          span.append(img);
-          var h2 = document.createElement('h2');
-          h2.className = "card-genres";
-          h2.textContent = current.show.genres;
-          span.append(h2);
-          var newDiv = document.createElement('div');
-          newDiv.className = 'card-summary';
-          span.append(newDiv);
-          var em = document.createElement('em');
-          newDiv.append(em);
-          var p = document.createElement('p');
-          p.innerHTML = current.show.summary;
-          newDiv.append(p);
-          var a = document.createElement('a');
-          a.href = current.show.url;
-          a.innerText = "View Show";
-          newDiv.append(a);
-         results.append(span);
+    $.get(`https://digimon-api.vercel.app/api/digimon/name/${inputText}`, function (data) {
+        if (inputText === "") {
+            alert("Please input a Digimon name!");
         }
-      }
-     
+        else {
+            console.log(data);
+            var results = document.querySelector('#results');
+            results.innerHTML = "";
+            for (var i = 0; i < data.length; i++) {
+                var current = data[i];
+
+                var span = document.createElement('span');
+                var h3 = document.createElement('h3');
+                h3.className = "card-title";
+                h3.textContent = current.name;
+                span.className = 'result-card';
+                span.append(h3);
+                var img = document.createElement('img');
+                img.className = 'card-image';
+                img.setAttribute('src', `${current.img}`);
+                span.append(img);
+                var h2 = document.createElement('h2');
+                h2.className = "card-level";
+                h2.textContent = current.level;
+                span.append(h2);
+                results.append(span);
+            }
+        }
+
     })
-  
-  }
+
+}
+const searchlvlBtn = document.getElementById('levelSubmit')
+searchlvlBtn.addEventListener("click", userlvlinput)
+
+function userlvlinput() {
+    const inputLevel = document.getElementById("levelSelect").value;
+    $.get(`https://digimon-api.vercel.app/api/digimon/level/${inputLevel}`, function (data2) {
+   
+            console.log(data2);
+            var results = document.querySelector('#results');
+            results.innerHTML = "";
+            for (var i = 0; i < data2.length; i++) {
+                var current2 = data2[i];
+
+                var span = document.createElement('span');
+                var h3 = document.createElement('h3');
+                h3.className = "card-title";
+                h3.textContent = current2.name;
+                span.className = 'result-card';
+                span.append(h3);
+                var img = document.createElement('img');
+                img.className = 'card-image';
+                img.setAttribute('src', `${current2.img}`);
+                span.append(img);
+                var h2 = document.createElement('h2');
+                h2.className = "card-level";
+                h2.textContent = current2.level;
+                span.append(h2);
+                results.append(span);
+            }
+        
+    })
+
+}
